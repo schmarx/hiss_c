01a4f8b43146e808d402972df349f483b53a664f
24388408 <24388408@sun.ac.za>
2024-05-14 14:50:53 +0200

    Updates to printing

--- hissy.c
+++ hissy.c
@@ -1,8 +1,9 @@
 
-#include <ncurses/curses.h>
+#include <locale.h>
 #include <ncurses/ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 
 int width = 10;
 int height = 10;
@@ -26,42 +27,146 @@ vect velocity;
 
 int iterations = 0;
 
-char *yellow = "\033[103m";
-char *blue = "\033[104m";
+char *yellow = "\033[93m";
+char *blue = "\033[94m";
 char *reset = "\033[0m";
+char *underline = "\033[4m";
+
+// wchar_t middle_vertical = L'\u2551';   // ║
+// wchar_t middle_horizontal = L'\u2550'; // ═
+// wchar_t top_right = L'\u2557';		   // ╗
+// wchar_t bottom_right = L'\u255d';	   // ╝
+// wchar_t bottom_left = L'\u255a';	   // ╚
+// wchar_t top_left = L'\u2554';		   // ╔
+
+wchar_t middle_vertical = 186;	 // ║
+wchar_t middle_horizontal = 205; // ═
+wchar_t top_right = 187;		 // ╗
+wchar_t bottom_right = 188;		 // ╝
+wchar_t bottom_left = 200;		 // ╚
+wchar_t top_left = 201;			 // ╔
 
 void draw(void) {
 	clear();
 
-	printw(" ");
-	for (int i = 0; i < width; i++) {
-		printw("__");
+	addch(ACS_ULCORNER);
+	char *title = "hissy";
+
+	for (int i = 0; i < width - 2; i++) {
+		addch(ACS_HLINE);
 	}
-	printw("_\n");
+	for (int i = 0; i < 5; i++) {
+		addch(title[i]);
+	}
+	for (int i = 0; i < width - 3; i++) {
+		addch(ACS_HLINE);
+	}
+	addch(ACS_HLINE);
+	addch(ACS_URCORNER);
+	addch('\n');
 
 	for (int i = 0; i < height; i++) {
-		printw("|");
+		addch(ACS_VLINE);
 		for (int j = 0; j < width; j++) {
 			int has = 0;
+
 			for (body *hiss_current = hiss; hiss_current != NULL; hiss_current = hiss_current->next) {
 				if (i == hiss_current->pos.y && j == hiss_current->pos.x) {
+					char ch = ' ';
+
+					attron(COLOR_PAIR(1));
+
+					if (hiss_current == hiss) {
+						addch(' ');
+						addch('H');
+					} else if (hiss_current->pos.y == hiss_current->prev->pos.y && hiss_current->pos.y == hiss_current->next->pos.y) {
+						addch(ACS_HLINE);
+						addch(ACS_HLINE);
+					} else {
+						if (hiss_current->pos.x == hiss_current->prev->pos.x && hiss_current->pos.x == hiss_current->next->pos.x) {
+							addch(' ');
+							addch(ACS_VLINE);
+						} else if (hiss_current->pos.x == hiss_current->prev->pos.x && hiss_current->pos.y < hiss_current->prev->pos.y && hiss_current->pos.x < hiss_current->next->pos.x) {
+							addch(' ');
+							addch(ACS_ULCORNER);
+						} else if (hiss_current->pos.x == hiss_current->prev->pos.x && hiss_current->pos.y > hiss_current->prev->pos.y && hiss_current->pos.x < hiss_current->next->pos.x) {
+							addch(' ');
+							addch(ACS_LLCORNER);
+						} else if (hiss_current->pos.y == hiss_current->prev->pos.y && hiss_current->pos.x < hiss_current->prev->pos.x && hiss_current->pos.y < hiss_current->next->pos.y) {
+							addch(' ');
+							addch(ACS_ULCORNER);
+						} else if (hiss_current->pos.y == hiss_current->prev->pos.y && hiss_current->pos.x < hiss_current->prev->pos.x && hiss_current->pos.y > hiss_current->next->pos.y) {
+							addch(' ');
+							addch(ACS_LLCORNER);
+						}
+
+						else if (hiss_current->pos.x == hiss_current->prev->pos.x && hiss_current->pos.y > hiss_current->prev->pos.y && hiss_current->pos.x > hiss_current->next->pos.x) {
+							addch(ACS_HLINE);
+							addch(ACS_LRCORNER);
+						} else if (hiss_current->pos.x == hiss_current->prev->pos.x && hiss_current->pos.y < hiss_current->prev->pos.y && hiss_current->pos.x > hiss_current->next->pos.x) {
+							addch(ACS_HLINE);
+							addch(ACS_URCORNER);
+						} else if (hiss_current->pos.y == hiss_current->prev->pos.y && hiss_current->pos.x > hiss_current->prev->pos.x && hiss_current->pos.y < hiss_current->next->pos.y) {
+							addch(ACS_HLINE);
+							addch(ACS_URCORNER);
+						} else if (hiss_current->pos.y == hiss_current->prev->pos.y && hiss_current->pos.x > hiss_current->prev->pos.x && hiss_current->pos.y > hiss_current->next->pos.y) {
+							addch(ACS_HLINE);
+							addch(ACS_LRCORNER);
+						} else addch('O');
+					}
+
+					// if (hiss_current == hiss) printw(" %c", 'H');
+					// else if (hiss_current->next->pos.x == hiss_current->pos.x) {
+					// 	addch(' ');
+					// 	if (hiss_current->prev->pos.x == hiss_current->pos.x) addch(ACS_VLINE);
+					// 	else if (hiss_current->prev->pos.x > hiss_current->prev->pos.x) addch(ACS_LLCORNER);
+					// 	else addch(ACS_LRCORNER);
+					// } else if (hiss_current->next->pos.x == hiss_current->pos.x) {
+					// 	addch(' ');
+					// 	addch(ACS_VLINE);
+					// } else if (hiss_current->next->pos.y == hiss_current->pos.y) {
+					// 	addch(ACS_HLINE);
+					// 	addch(ACS_HLINE);
+					// } else printw(" %c", 'h');
+
+					attroff(COLOR_PAIR(1));
 					has = true;
-					printw(" h");
 					break;
 				}
 				if (hiss_current == hiss_tail) break;
 			}
-			if (i == food.y && j == food.x) printw(" o", yellow, reset);
-			else if (!has) printw("  ");
+			if (i == food.y && j == food.x) {
+				attron(COLOR_PAIR(2));
+				printw(" o", yellow, reset);
+				attroff(COLOR_PAIR(2));
+			} else if (!has) printw("  ");
 		}
-		printw(" |\n");
+		addch(' ');
+		addch(ACS_VLINE);
+		addch('\n');
 	}
 
-	printw("|");
+	addch(ACS_LLCORNER);
 	for (int i = 0; i < width; i++) {
-		printw("__");
+		addch(ACS_HLINE);
+		addch(ACS_HLINE);
 	}
-	printw("_|\n");
+	addch(ACS_HLINE);
+	addch(ACS_LRCORNER);
+	addch('\n');
+}
+
+void add(void) {
+	body *hiss_next = malloc(sizeof(body));
+	hiss_next->pos.x = hiss_tail->pos.x;
+	hiss_next->pos.y = hiss_tail->pos.y;
+	hiss_next->next = hiss;
+	hiss_next->prev = hiss_tail;
+
+	hiss->prev = hiss_next;
+
+	hiss_tail->next = hiss_next;
+	hiss_tail = hiss_next;
 }
 
 void place_food(void) {
@@ -79,9 +184,14 @@ void free_hiss() {
 }
 
 int main(int argc, char *argv[]) {
+
 	initscr();
 	noecho();
 	srand(clock());
+
+	start_color();
+	init_pair(1, COLOR_BLUE, COLOR_BLACK);
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
 
 	hiss = malloc(sizeof(body));
 	hiss->pos.x = 0;
@@ -127,17 +237,7 @@ int main(int argc, char *argv[]) {
 		if (hiss->pos.y >= height) hiss->pos.y = 0;
 
 		if (hiss->pos.x == food.x && hiss->pos.y == food.y) {
-			body *hiss_next = malloc(sizeof(body));
-			hiss_next->pos.x = hiss_tail->pos.x;
-			hiss_next->pos.y = hiss_tail->pos.y;
-			hiss_next->next = hiss;
-			hiss_next->prev = hiss_tail;
-
-			hiss->prev = hiss_next;
-
-			hiss_tail->next = hiss_next;
-			hiss_tail = hiss_next;
-
+			add();
 			place_food();
 		}
 
